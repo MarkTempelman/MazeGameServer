7c81e601f82c0dee75aded4689bfbd0f0b62ee81
MarkTempelman <43167879+MarkTempelman@users.noreply.github.com>
2020-12-08 14:49:44 +0100

    Project init

---
+++ src/main/java/nl/fhict/mazegameserver/MazegameserverApplication.java
@@ -0,0 +1,12 @@
+package nl.fhict.mazegameserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MazegameserverApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MazegameserverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/fhict/mazegameserver/MazegameserverApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.fhict.mazegameserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MazegameserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
