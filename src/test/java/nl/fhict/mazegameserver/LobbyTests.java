4daeb57e6cc5477f809d3af772b31271ed01a529
MarkTempelman <43167879+MarkTempelman@users.noreply.github.com>
2021-01-04 13:58:28 +0100

    added first test

--- src/test/java/nl/fhict/mazegameserver/LobbyTests.java
+++ src/test/java/nl/fhict/mazegameserver/LobbyTests.java
@@ -1,4 +1,30 @@
 package nl.fhict.mazegameserver;
 
+import nl.fhict.mazegameserver.models.Lobby;
+import nl.fhict.mazegameserver.models.Player;
+import nl.fhict.mazegameserver.services.LobbyService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
 public class LobbyTests {
+    private LobbyService lobbyService;
+    private Player player;
+
+    @BeforeEach
+    public void setup(){
+        lobbyService = new LobbyService();
+        player = new Player();
+        player.setId(1);
+    }
+
+    @Test
+    public void TestJoinLobbyWhenNoneExist(){
+        Lobby lobby = lobbyService.joinRandomLobby(player);
+        assertEquals(lobby.getLobbyId(), 1);
+    }
 }
